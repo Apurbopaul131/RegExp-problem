4e888fa66917ebc5751973a54c2071ce5ee3ffb5
Apurbopual131 <apurbopaul131@gmail.com>
2023-06-30 17:44:11 +0600

    regular expression problem

---
+++ js/regExp.js
@@ -0,0 +1,23 @@
+// const text = `My name is Apurbo paul.My father name is Shanti Paul`;
+// const searchStr = "MY";
+// returns the position of the string
+// const postionStr = text.search(/my/i);
+//relace the string using string
+//The replace method returns modified string after replace
+// const replaceStr = text.replace("APURBO","Simanto");
+// console.log(text);
+// console.log(replaceStr);
+//Replace string using regular expression
+// 
+// console.log(text.replace(/apurbo paul/i),"Sajal Paul");
+// let text2 = "\nIs th\nis it?";
+//g is an modifiers that that provides all match string array.
+// let result = text.match(/is/g);
+// console.log(result);
+// let text3 = "17 is birthday of national leader sheikh Mujib.";
+// let result1 = text3.match(/[1-7]/g);
+// console.log(result1);
+// const text = "Apurbo is a very very nice awesome boy.";
+// const regExp = /very/g;
+// console.log(text.replace("very","VERY"));
+
